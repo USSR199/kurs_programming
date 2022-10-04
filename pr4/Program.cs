b16077a89116818ea675b985da01962e89da7a3c
Andrew <andgorodnichev8@gmail.com>
2022-10-04 19:52:45 +0300

    создали 4 программу

---
+++ pr4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+ int maximum = num1;
+if (num2>maximum){
+    maximum = num2;
+    }
+if (num3>maximum){
+    maximum = num3;
+    }
+Console.WriteLine($" большее из трех чисел {maximum} ");
