6d0ab3d948ed242ea6962b9b46703629a13d2156
Andrew <andgorodnichev8@gmail.com>
2022-10-04 21:09:24 +0300

    создали 8  программу

---
+++ pr8/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+class Program
+{
+    static void Main(string[] args)
+    */
+        Console.Write("Введите количество чисел: ");
+        int n = int.Parse(Console.ReadLine());
+       
+               for (int i = 1; i <= n; i++)
+        {
+            if (i % 2 == 0) 
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+    
+
