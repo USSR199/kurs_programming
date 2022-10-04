7e33b2f40dfa0bd7da5d53e9d51a4a43b007c0ee
Andrew <andgorodnichev8@gmail.com>
2022-10-04 20:15:16 +0300

    создали 6  программу

---
+++ pr6/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2==0){
+   Console.WriteLine("Число четное");
+    }
+
