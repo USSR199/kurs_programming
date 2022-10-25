88018ecd975bc5cc99bd3cce8e0d501c1148d351
Andrew <andgorodnichev8@gmail.com>
2022-10-25 08:21:23 +0300

    создали 43  программу

---
+++ 6 sem/pr43/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите коэффициент k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите коэффициент k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double x = (b2-b1)/(k1-k2);
+double y = k1*x+b1;
+Console.WriteLine ("Точка пересечения 2-х прямых с заданными коэффициентами k и b имеет координаты:  ("+x+";"+y+")");
